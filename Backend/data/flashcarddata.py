114db797fc12d45e786ccd0a681f29a50bdea80c
pujakadayatdata <pujakadayat1@gmail.com>
2025-08-18 22:57:31 +0545

    added basic endpoints

---
+++ Backend/data/flashcarddata.py
@@ -0,0 +1,14 @@
+
+
+flashcards = {
+   1: {
+        "word": "Namaste",
+        "meaning": "A respectful greeting with palms pressed together",
+        "example": "She greeted her teacher with a warm namaste.",
+        "cultural_context": "Traditional Nepalese greeting showing respect and humility",
+        "image": "https://www.pinterest.com/pin/premium-vector--720364902920899300/",
+        "difficulty": "easy"
+    },
+}
+   
+ 
